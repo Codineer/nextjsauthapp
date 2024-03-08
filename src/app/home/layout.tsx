3c3a0bfe793ba159587a5358872238355f8fe5c1
Codineer <utkarshgamer294@outlook.com>
2024-03-08 20:00:15 +0530

    commit

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -56,17 +56,19 @@ export default function MusicPageLayout({
             ]}>
                 <div className="" >
                     <div className='w-screen bg-custom-background-main bg-cover bg-center bg-no-repeat h-screen bg-black text-white grid gap-2 p-2' style={{ gridTemplateColumns: "350px 1fr" }}>
-                        <div className='h-full border border-white p-2 flex flex-col gap-2'>
-                            <div className="w-full h-auto p-2 border border-white">
-                                <Image alt="" quality="100" width={100} height={20} src="/images/logo.png" />
-                                <Link href={"/home"}>
-
+                        <div className='h-full border border-white p-2 flex flex-col gap-2 '>
+                            <div className="w-full h-auto p-2 border border-white flex flex-col gap-2">
+                                <Link className="cursor-pointer" href={"/home"}>
+                                    <Image alt="" quality="100" width={100} height={20} src="/images/logo.png" />
+                                </Link>
+                                <Link className="cursor-pointer" href={"/home"}>
                                     <h1>Home</h1>
                                 </Link>
-                                <Link href={"/home/albums"}>
+                                <Link className="cursor-pointer" href={"/home/albums"}>
 
                                     <h1>Albums</h1>
                                 </Link>
+                                <h1>Liked Songs</h1>
                             </div>
                             <div className="h-full w-full p-2 border border-white">
                                 <div className="text-2xl font-semibold">Current Album</div>
