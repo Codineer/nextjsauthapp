904e4de58c44c2bbc091512706c6b6f15fd271cf
Codineer <utkarshgamer294@outlook.com>
2024-03-16 13:30:44 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+    pages: {
+        // If 'app' is a subdirectory of the root
+        '/': { page: '/app' },
+
+        // If 'app' is the root directory
+        '/': { page: '/' },
+    },
     // Add other configuration options as needed
     webpack: (config) => {
         config.externals = [...config.externals, 'bcrypt'];
